6d8dc23afa9dd3a6cb4f38ed416fe1f005629355
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-28 14:01:57 -0700

    TaskModal component created

---
+++ js-hackathon-olympics/src/components/TaskModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class TaskModal extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <div className='backdrop d-flex'>
+        <div className='modal-contents m-auto bg-white border rounded p-3 task-modal'>
+          <h3 className='text-center'>this.props.task.title</h3>
+          <p className='text-center'>this.props.task.description</p>
+        </div>
+      </div>
+    )
+  }
+
+}
