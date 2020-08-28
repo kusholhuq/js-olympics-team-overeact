bce402949ae22fe3f10dc2a254437c80ccb0d324
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-28 15:30:35 -0700

    close button added, close modal method added, open modal method added

--- js-hackathon-olympics/src/components/ColumnContainer.jsx
+++ js-hackathon-olympics/src/components/ColumnContainer.jsx
@@ -6,8 +6,28 @@ export default class ColumnContainer extends React.Component {
     super(props);
     this.state = {
       columnCount: 3,
-      showModal: true
+      showModal: true,
+      selectedTaskDetails:{
+        title:'',
+        description:''
+      }
     }
+    this.closeModal = this.closeModal.bind(this);
+    this.getTaskDetails = this.getTaskDetails.bind(this);
+  }
+
+  closeModal(){
+    this.setState({showModal:false})
+  }
+
+  getTaskDetails(task){
+    this.setState({
+      selectedTaskDetails:{
+        title:task.title,
+        description:task.description
+      },
+      showModal:true
+    });
   }
 
   render() {
