125cc1b5f359de2ffa1935b0aa2f4258659b557e
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-28 13:08:49 -0700

    typo in constructor

--- js-hackathon-olympics/src/components/ColumnContainer.jsx
+++ js-hackathon-olympics/src/components/ColumnContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default class ColumnContainer extends React.Component {
-  construct(props){
+  constructor(props){
     super(props);
     this.state = {
       columnCount: 3
