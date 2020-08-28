b3053c99f92c8239b01e307dc8dda993a95f59a0
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-28 13:07:08 -0700

    imported bootstrap cdn, created ColumnContainer component

--- js-hackathon-olympics/src/App.js
+++ js-hackathon-olympics/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
+import Header from './components/Header';
+import ColumnContainer from './components/ColumnContainer';
 
 function App() {
   return (
     <div>
       <Header/>
+      <ColumnContainer/>
     </div>
   );
 }
