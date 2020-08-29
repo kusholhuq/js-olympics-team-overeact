8e5356e5306b98a10d6eb7bea4eb60ae6d03d7e8
Jason Kim <thehesedlife@gmail.com>
2020-08-28 22:39:54 -0700

    added a basic functionality of the nested drag and drop for React

--- js-hackathon-olympics/src/components/Column.jsx
+++ js-hackathon-olympics/src/components/Column.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import TaskCard from './TaskCard';
 
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
 export default class Column extends React.Component {
   constructor(props) {
     super(props);
@@ -9,24 +24,23 @@ export default class Column extends React.Component {
   }
 
   render() {
+    const { title, subItems, columnId, parentProvided, parentSnapshot } = this.props;
+
     return (
-      <div className='w-25 vh-100 border'>
-        <header className='border mx-0 w-100 d-flex justify-content-center'>
-          {this.props.title}
-        </header>
-        <div className='mx-0 w-100 d-flex justify-content-center'>
-          {
-          this.props.tasks.map(content=>{
-            return (
-              <TaskCard
-              key={content.taskId}
-              content={content.task}
-              />
-            )
-          })
-          }
+      <div className="m-3">
+        <div
+          className="border"
+          ref={parentProvided.innerRef}
+          {...parentProvided.draggableProps}
+          style={getItemStyle(parentSnapshot.isDragging, parentProvided.draggableProps.style)}
+        >
+          <header className="border mx-0 w-100 d-flex justify-content-center" {...parentProvided.dragHandleProps}>
+            {title}
+          </header>
+          <TaskCard subItems={subItems} columnId={columnId} />
         </div>
+        {parentProvided.placeholder}
       </div>
-    )
+    );
   }
 }
--- js-hackathon-olympics/src/components/TaskCard.jsx
+++ js-hackathon-olympics/src/components/TaskCard.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
-export default function TaskCard(props){
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  width: "120px",
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+  display: "inline-flex",
+  padding: "10px",
+  margin: "0 10px 10px 0",
+  border: "1px solid grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  margin: "10px 0",
+});
+
+export default function TaskCard(props) {
   return (
-    <div className="border">
-      {props.content}
-    </div>
-  )
+    <Droppable droppableId={props.columnId} type={`droppableSubItem`}>
+      {(provided, snapshot) => (
+        <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
+          {props.subItems.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided, snapshot) => (
+                <div style={{ display: "flex" }}>
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                  >
+                    <h5 {...provided.dragHandleProps}>{item.title}</h5>
+                    <button type="button">X</button>
+                  </div>
+                  {provided.placeholder}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
 }
