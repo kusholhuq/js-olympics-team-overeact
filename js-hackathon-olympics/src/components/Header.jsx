5c6560b336d2031253f6276d07f02f22ed95f9c0
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-30 19:16:29 -0700

    some styles updated

--- js-hackathon-olympics/src/components/Header.jsx
+++ js-hackathon-olympics/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(props){
   return (
     <div className="d-flex justify-content-between header mb-5 position-fixed">
-      <h1 className="ml-5 mt-3 mb-2 pac white">Kanvas</h1>
+      <h1 className="ml-5 mt-3 mb-2 mont white"><i className="fas fa-paint-brush blue"></i>  Kanvas</h1>
       <h4 className="mr-5 mt-3 mb-2">
         <i onClick={() => props.handleDisplay()} className="fas fa-question-circle help white"></i>
       </h4>
--- js-hackathon-olympics/src/components/LandingPage.jsx
+++ js-hackathon-olympics/src/components/LandingPage.jsx
@@ -2,39 +2,51 @@ import React from "react";
 
 export default function Landing(props) {
 
-    return(
-      <div className="w-100 d-flex justify-content-center flex-wrap">
-        <h1 className="d-flex justify-content-center flex-wrap">Get organized, and Stay ahead of the competition with our new Kanban Board!</h1>
-        <div className='border greyBackground d-flex justify-content-center w-100'>
-          <img src="./kanbanBoard.png" alt="kanban board" className='py-3 w-75'/>
+  return (
+    <div className=" pt-5 light-grey">
+      <div className="d-flex">
+        <div className="ml-5 pl-5">
+          <h1 className="pac title-font">Kanvas</h1>
+          <h1 className="mont">by Team Ove<span className="blue">React</span></h1>
+          <p className="mont">Get organized, and Stay ahead of the competition with our new Kanban Board!</p>
+          <div>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+            <img src="" alt=""/>
+          </div>
         </div>
-        <div className='d-flex justify-content-center flex-wrap'>
-          <h2 className='w-100 d-flex justify-content-center'>What Our App has to Offer:</h2>
-          <ul>
-            <li>
-              Do you like to plan with more than 3 stages? You're in luck!
-              Our app gives you the ability to add and remove columns with ease.
-            </li>
-            <li>
-              We give you the power, the titles of the columns,
-              task titles and task contents are all completely editable.
-            </li>
-            <li>
-              Add new tasks to any Column at any time!
-            </li>
-            <li>
-              Thanks to the work of our developers, our app gives your the ability drag columns to switch the order if needed
-              and you can move tasks up and down within the column or horizontally to another column!
-            </li>
-          </ul>
-        </div>
-        <div className='d-flex justify-content-center flex-wrap'>
-          <h4 className='w-100'>So what are you waiting for, give our Kanban board a test drive by clicking the button below</h4>
-          <button onClick={()=>props.handleDisplay()} className='bg-info text-light w-25 border border-dark rounded'>
-            Enter Here
-          </button>
+
+        <div className="d-flex justify-content-center">
+          <img src="./kanbanBoard.png" alt="kanban board" className='py-3 w-75' />
         </div>
       </div>
-    )
+      <div className='d-flex justify-content-center flex-wrap'>
+        <h2 className='w-100 d-flex justify-content-center'>What Our App has to Offer:</h2>
+        <ul>
+          <li>
+            Do you like to plan with more than 3 stages? You're in luck!
+            Our app gives you the ability to add and remove columns with ease.
+            </li>
+          <li>
+            We give you the power, the titles of the columns,
+            task titles and task contents are all completely editable.
+            </li>
+          <li>
+            Add new tasks to any Column at any time!
+            </li>
+          <li>
+            Thanks to the work of our developers, our app gives your the ability drag columns to switch the order if needed
+            and you can move tasks up and down within the column or horizontally to another column!
+            </li>
+        </ul>
+      </div>
+      <div className='d-flex justify-content-center flex-wrap'>
+        <h4 className='w-100'>So what are you waiting for, give our Kanban board a test drive by clicking the button below</h4>
+        <button onClick={() => props.handleDisplay()} className='bg-info text-light w-25 border border-dark rounded'>
+          Enter Here
+          </button>
+      </div>
+    </div>
+  )
 
 }
